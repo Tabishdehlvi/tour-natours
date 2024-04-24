7874ac6daf256dcfa2f001801d1b9c79f7acfd07
Your Name <thdevthree@gmial.com>
2024-04-24 14:16:01 +0500

    add stripe aleer box on succesful 1

--- public/js/index.js
+++ public/js/index.js
@@ -74,4 +74,4 @@ if (loginOutBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 20);
+if (alertMessage) showAlert('success', alertMessage, 20);
